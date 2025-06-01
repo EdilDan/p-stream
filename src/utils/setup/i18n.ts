bf82eafc6485f85a2331ab085c6d49574722102a
EdilDan <88191638+EdilDan@users.noreply.github.com>
2025-06-01 19:16:08 +0300

    Update i18n.ts

--- src/utils/setup/i18n.ts
+++ src/utils/setup/i18n.ts
@@ -10,7 +10,8 @@ const resources = Object.fromEntries(
   Object.entries(locales).map((entry) => [entry[0], { translation: entry[1] }]),
 );
 i18n.use(initReactI18next).init({
-  fallbackLng: "en",
+  fallbackLng: "am",
+  lng: "am", // Set Amharic as the default language
   resources,
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
